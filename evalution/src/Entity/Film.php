b0cc96d70c01ac0b79e6d06b4d8158834225dee7
Samuel Lacaille <samuel76560@outlook.fr>
2022-01-07 10:27:40 +0100

    creation entité film et migration

---
+++ evalution/src/Entity/Film.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FilmRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=FilmRepository::class)
+ */
+class Film
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $Synopsis;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Type;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $Date_creation;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $Id): self
+    {
+        $this->Id = $Id;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getSynopsis(): ?string
+    {
+        return $this->Synopsis;
+    }
+
+    public function setSynopsis(?string $Synopsis): self
+    {
+        $this->Synopsis = $Synopsis;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->Type;
+    }
+
+    public function setType(string $Type): self
+    {
+        $this->Type = $Type;
+
+        return $this;
+    }
+
+    public function getDateCreation(): ?\DateTimeInterface
+    {
+        return $this->Date_creation;
+    }
+
+    public function setDateCreation(\DateTimeInterface $Date_creation): self
+    {
+        $this->Date_creation = $Date_creation;
+
+        return $this;
+    }
+}
